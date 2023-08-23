154c158fe5427beac6445905d6e0f842a9b08240
madhukarwakhare <88505890+madhukarwakhare@users.noreply.github.com>
2023-08-23 18:04:40 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="mlproject",
     version="0.0.1",
-    author="Krish",
-    author_email="krishnaik06@gmail.com",
+    author="Madhukar",
+    author_email="madhukarwakhare@gmail.com",
     packages=find_packages(),
     install_requires=[],
 )
